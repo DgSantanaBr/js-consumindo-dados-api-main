fc7fd9246bd9fbaa8d0fa18b612eb8dbe3e5c4c7
DgSantanaBR <diiiego.santana2@gmail.com>
2023-07-12 13:39:51 -0300

    projeto-finalizado

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+// ------------------------------------------
 //interação com a API
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/');
-
+var consultaCep = fetch('https://viacep.com.br/ws/0100000/json/')
+//então converte a requisição em json
+.then(resposta => resposta.json())
+// então imprime a resposta no console
+.then(r => console.log(r))
+//pega o erro e o imprime no console caso a requisição com o then dê erro
+.catch(erro => console.log(erro));
+// ------------------------------------------
 console.log(consultaCep)
